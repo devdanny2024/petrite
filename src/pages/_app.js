d76ed3a5f1d95794d8c70fba2422a3c314d22d31
Blvckcodeio <ninokayglobal@gmail.com>
2024-10-01 06:22:18 +0100

    New Project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,12 @@
 import "@/styles/globals.css";
+import "@/styles/Banner.css";
+import "@/styles/Features.css";
+import "@/styles/Footer.css";
+import "@/styles/Heroheader.css";
+import "@/styles/Navbar.css";
+import "@/styles/Products.css";
+import "@/styles/Reviews.css";
+import "@/styles/Services.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
